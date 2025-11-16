4359aace830624b1361e1af876bd0fe37ed7144d
Joni Koskela <joni.koskela2@gmail.com>
2025-11-16 18:13:22 +0200

    Add files via upload

--- h3a/main.cpp
+++ h3a/main.cpp
@@ -1,37 +1,21 @@
-#include "chef.h"
 #include <iostream>
+#include "chef.h"
+using namespace std;
 
-// Chef-luokan toteutukset
-Chef::Chef(std::string n) {
-    name = n;
-    std::cout << "Chef " << name << " konstruktori" << std::endl;
+
+int main() {
+
+    Chef chef("Gordon Ramsay");
+    chef.makeSalad();
+    chef.makeSoup();
+
+    ItalianChef italianChef("Anthony Bourdain");
+    italianChef.makeSalad();
+    italianChef.makeSoup();
+    italianChef.makePasta();
+    std::cout << "The name of the Italian Chef is " << italianChef.getName() << std::endl;
+
+    return 0;
 }
 
-Chef::~Chef() {
-    std::cout << "Chef " << name << " destruktori" << std::endl;
-}
 
-void Chef::makeSalad() {
-    std::cout << "Chef " << name << " makes salad" << std::endl;
-}
-
-void Chef::makeSoup() {
-    std::cout << "Chef " << name << " makes soup" << std::endl;
-}
-
-// ItalianChef-luokan toteutukset
-ItalianChef::ItalianChef(std::string n) : Chef(n) {
-    std::cout << "ItalianChef " << name << " konstruktori" << std::endl;
-}
-
-ItalianChef::~ItalianChef() {
-    std::cout << "ItalianChef " << name << " destruktori" << std::endl;
-}
-
-std::string ItalianChef::getName() {
-    return name;
-}
-
-void ItalianChef::makePasta() {
-    std::cout << "ItalianChef " << name << " makes pasta" << std::endl;
-}
